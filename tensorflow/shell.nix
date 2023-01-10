e5f7cf25ad4e14f94f434456127591a4aa114366
shuiruge <shuiruge@whu.edu.cn>
2023-01-10 09:13:07 +0800

    Change Python version.

--- tensorflow/shell.nix
+++ tensorflow/shell.nix
@@ -1,8 +1,9 @@
 # shell.nix
 # Reference: https://nixos.wiki/wiki/Tensorflow
+
 with import <nixpkgs> { };
 let
-  pythonPackages = python37Packages;
+  pythonPackages = python39Packages;
 in pkgs.mkShell rec {
   name = "tensorflowEnv";
   venvDir = "./.venv";
@@ -21,11 +22,14 @@ in pkgs.mkShell rec {
   postShellHook = ''
     # Setup TensorFlow environment.
     export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.cudaPackages_10_1.cudatoolkit}/lib:${pkgs.cudaPackages_10_1.cudatoolkit}/lib:${pkgs.cudaPackages_10_1.cudatoolkit.lib}/lib:$LD_LIBRARY_PATH
-    alias pip="PIP_PREFIX='$(pwd)/_build/pip_packages' TMPDIR='$HOME' \pip"
-    export PYTHONPATH="$(pwd)/_build/pip_packages/lib/python3.7/site-packages:$PYTHONPATH"
-    export PATH="$(pwd)/_build/pip_packages/bin:$PATH"
+    alias pip="PIP_PREFIX='$(pwd)/.build/pip_packages' TMPDIR='$HOME' \pip"
+    export PYTHONPATH="$(pwd)/.build/pip_packages/lib/python3.9/site-packages:$PYTHONPATH"
+    export PATH="$(pwd)/.build/pip_packages/bin:$PATH"
     unset SOURCE_DATE_EPOCH
+
     # Setup local virtual environment.
     pip install . -r requirements.txt
+    # Or in China.
+    # pip install . -r requirements.txt -i https://pypi.douban.com/simple
   '';
 }
