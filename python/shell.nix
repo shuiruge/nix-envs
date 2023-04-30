b29ee91e0cf47e12feba6e47b0c1aaa6436431da
shuiruge <shuiruge@whu.edu.cn>
2023-04-30 15:52:25 +0800

    Fix.

--- python/shell.nix
+++ python/shell.nix
@@ -28,7 +28,9 @@ in pkgs.mkShell rec {
   # Now we can execute any commands within the virtual environment.
   # This is optional and can be left out to run pip manually.
   postShellHook = ''
-    pip install --no-cache-dir -r requirements.txt
+    if [ ! -d .venv ]; then
+      pip install --no-cache-dir -r requirements.txt
+    fi
   '';
 
 }
